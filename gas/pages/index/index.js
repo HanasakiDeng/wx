a0463cc1db18b19e934670b503d46c6e7fcae4af
gary <2826369060@qq.com>
2017-09-13 17:32:28 +0800

    代码更新_20170913

--- gas/pages/index/index.js
+++ gas/pages/index/index.js
@@ -1,64 +1,96 @@
+import { Config } from '../../utils/config.js';
 var markersData = [];
-var amapFile = require('../../libs/amap-wx.js');//如：..­/..­/libs/amap-wx.js
+var amapFile = require('../../libs/amap-wx.js');//如：..­/..­/libs/amap-wx.js;
+
 Page({
   data: {
-    markers: [],
-    latitude: '',
-    longitude: '',
-    textData: {}
-  },
-  makertap: function (e) {
-    var id = e.markerId;
-    var that = this;
-    that.showMarkerInfo(markersData, id);
-    that.changeMarkerColor(markersData, id);
+    markers: [{
+      iconPath: "../../img/mapicon_navi_s.png",
+      id: 0,
+      latitude: 39.989643,
+      longitude: 116.481028,
+      width: 23,
+      height: 33
+    }, {
+      iconPath: "../../img/mapicon_navi_e.png",
+      id: 0,
+      latitude: 39.90816,
+      longitude: 116.434446,
+      width: 24,
+      height: 34
+    }],
+    distance: '',
+    cost: '',
+    polyline: []
   },
   onLoad: function () {
     var that = this;
-    var myAmapFun = new amapFile.AMapWX({ key: '63b6702375a2cf6b14c6ba5340ccd1a3' });
-    myAmapFun.getPoiAround({
-      iconPathSelected: '选中 marker 图标的相对路径', //如：..­/..­/img/marker_checked.png
-      iconPath: '未选中 marker 图标的相对路径', //如：..­/..­/img/marker.png
+    var myAmapFun = new amapFile.AMapWX({ key: Config.key });
+    myAmapFun.getDrivingRoute({
+      origin: '116.481028,39.989643',
+      destination: '116.434446,39.90816',
       success: function (data) {
+        var points = [];
         console.log(data);
+        if (data.paths && data.paths[0] && data.paths[0].steps) {
+          var steps = data.paths[0].steps;
+          for (var i = 0; i < steps.length; i++) {
+            var poLen = steps[i].polyline.split(';');
+            for (var j = 0; j < poLen.length; j++) {
+              points.push({
+                longitude: parseFloat(poLen[j].split(',')[0]),
+                latitude: parseFloat(poLen[j].split(',')[1])
+              });
+            }
+          }
+        }
+        that.setData({
+          polyline: [{
+            points: points,
+            color: "#0091ff",
+            width: 6
+          }]
+        });
+        if (data.paths[0] && data.paths[0].distance) {
+          that.setData({
+            distance: data.paths[0].distance + '米'
+          });
+        }
+        if (data.taxi_cost) {
+          that.setData({
+            cost: '打车约' + parseInt(data.taxi_cost) + '元'
+          });
+        }
 
       },
       fail: function (info) {
-        wx.showModal({ title: info.errMsg })
-      }
-    })
-    myAmapFun.getInputtips({
-      keywords: '加油站',
-      city: '上海市',
-      type: '生活服务',
-      success: function (data) {
-        console.log(data);
-      }
-    })
-  },
-  showMarkerInfo: function (data, i) {
-    var that = this;
-    that.setData({
-      textData: {
-        name: data[i].name,
-        desc: data[i].address
-      }
-    });
-  },
-  changeMarkerColor: function (data, i) {
-    var that = this;
-    var markers = [];
-    for (var j = 0; j < data.length; j++) {
-      if (j == i) {
-        data[j].iconPath = "选中 marker 图标的相对路径"; //如：..­/..­/img/marker_checked.png
-      } else {
-        data[j].iconPath = "未选中 marker 图标的相对路径"; //如：..­/..­/img/marker.png
-      }
-      markers.push(data[j]);
-    }
-    that.setData({
-      markers: markers
-    });
-  }
 
+      }
+    })
+  },
+  goDetail: function () {
+    wx.navigateTo({
+      url: '../navigation_car_detail/navigation'
+    })
+  },
+  goToCar: function (e) {
+    wx.redirectTo({
+      url: '../navigation_car/navigation'
+    })
+  },
+  goToBus: function (e) {
+    wx.redirectTo({
+      url: '../navigation_bus/navigation'
+    })
+  },
+  goToRide: function (e) {
+    wx.redirectTo({
+      url: '../navigation_ride/navigation'
+    })
+  },
+  goToWalk: function (e) {
+    wx.redirectTo({
+      url: '../navigation_walk/navigation'
+    })
+  }
 })
---
+++ gas/utils/config.js
@@ -0,0 +1,6 @@
+let Config = {
+  key: '63b6702375a2cf6b14c6ba5340ccd1a3'
+}
+export {
+  Config
+}
