37688cc7c452b8c2fa9829462c806f08c7d7a123
gary <2826369060@qq.com>
2017-09-14 17:33:27 +0800

    订单系统添加

---
+++ order/app.js
@@ -0,0 +1,20 @@
+//app.js
+var utilsPath = '../../utils';
+let loginFlag = wx.getStorageSync("LOGIN_FLAG");
+App({
+  onLaunch: function () {
+   
+  },
+  onShow: function () {
+    // wx.clearStorageSync();
+  },
+  onHide: function () {
+
+  },
+  onError: function () {
+
+  }
+
+});
+
+
