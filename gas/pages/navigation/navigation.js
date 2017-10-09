e9ff6f28656129804f74d76b02d43bf0a0c4ee67
gary <2826369060@qq.com>
2017-10-09 17:35:25 +0800

    图片添加

--- gas/pages/navigation/navigation.js
+++ gas/pages/navigation/navigation.js
@@ -1,66 +1,97 @@
-// pages/navigation/navigation.js
+import { Config } from '../../utils/config.js';
+var amapFile = require('../../libs/amap-wx.js');//如：..­/..­/libs/amap-wx.js;
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    distance: '',
+    polyline: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-  
+    if (options) {
+      this.init(options);
+    }
   },
-
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 初始化数据
    */
-  onReady: function () {
-  
+  init: function (options) {
+    let that = this;
+    let gasStation = JSON.parse(options.gasStation),
+      destination = gasStation.location.split(','),
+      originLocation = gasStation.originLocation,
+      originLocationArray = originLocation.split(',');
+    this.setData({
+      // 起点坐标
+      originLocation: originLocation,
+      currentLongitude: (parseFloat(originLocationArray[0]) + parseFloat(destination[0])) / 2.000000,
+      currentLatitude: (parseFloat(originLocationArray[1]) + parseFloat(destination[1])) / 2.000000,
+      destination: gasStation.location,
+      desLongitude: destination[0],
+      desLatitude: destination[1],
+
+      //起点终点图标标记
+      markers: [{
+        iconPath: "../../images/icon/start.png",
+        id: 0,
+        latitude: originLocationArray[1],
+        longitude: originLocationArray[0],
+        width: 23,
+        height: 33
+      }, {
+        iconPath: "../../images/icon/end.png",
+        id: 0,
+        latitude: destination[1],
+        longitude: destination[0],
+        width: 24,
+        height: 34
+      }],
+    })
+    this.getDrivingRoute();
   },
+  //获取驾车路线
+  getDrivingRoute: function () {
+    let that = this;
+    var myAmapFun = new amapFile.AMapWX({ key: Config.gdMapkey });
+    myAmapFun.getDrivingRoute({
+      origin: that.data.originLocation,
+      destination: that.data.destination,
+      success: function (data) {
+        console.log(data);
+        var points = [];
+        var instructions = [];
+        if (data.paths && data.paths[0] && data.paths[0].steps) {
+          var steps = data.paths[0].steps;
+          for (var i = 0; i < steps.length; i++) {
+            instructions.push(steps[i].instruction);
+            var poLen = steps[i].polyline.split(';');
+            for (var j = 0; j < poLen.length; j++) {
+              points.push({
+                longitude: parseFloat(poLen[j].split(',')[0]),
+                latitude: parseFloat(poLen[j].split(',')[1])
+              })
+            }
+          }
+        }
+        that.setData({
+          instructions: instructions,
+          polyline: [{
+            points: points,
+            color: "#0091ff",
+            width: 6
+          }]
+        });
+        if (data.paths[0] && data.paths[0].distance) {
+          that.setData({
+            distance: data.paths[0].distance + '米'
+          });
+        }
+      },
+      fail: function (info) {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  goDetail: function () {
+    console.log(this.data.instructions);
   }
 })
--- gas/pages/tpls/gas-station-tpl/gas-station-tpl.js
+++ gas/pages/tpls/gas-station-tpl/gas-station-tpl.js
@@ -13,32 +13,44 @@ class GasStation {
   getStationList() {
     let gdMap = new amapFile.AMapWX({ key: Config.gdMapkey });
     let self = this;
-    gdMap.getPoiAround({
-      querykeywords: '加油站',
-      querytypes: '010100',
-      success: function (data) {
-        console.log(data.poisData);
-        let gasStationList = [];
-        for (let item of data.poisData) {
-          // 地址处理 
-          item.address = item.cityname + item.adname + item.address;
-          if (item.distance >= 1000) {
-            item.unit = 'km';
-            item.distance = (item.distance / 1000).toFixed(2);
-          } else {
-            item.unit = 'm';
+    wx.getLocation({
+      success: function (res) {
+        gdMap.getPoiAround({
+          querykeywords: '加油站',
+          querytypes: '010100',
+          location: res.longitude + ',' + res.latitude,
+          success: function (data) {
+            console.log(data.poisData);
+            let gasStationList = [];
+            for (let item of data.poisData) {
+              // 地址处理 
+              item.address = item.cityname + item.adname + item.address;
+              item.originLocation = res.longitude + ',' + res.latitude
+              if (item.distance >= 1000) {
+                item.unit = 'km';
+                item.distance = (item.distance / 1000).toFixed(2);
+              } else {
+                item.unit = 'm';
+              }
+              gasStationList.push(item);
+            }
+            console.log(gasStationList);
+            self.context.setData({
+              gasStationList: gasStationList
+            })
           }
-          gasStationList.push(item);
-        }
-        console.log(gasStationList);
-        self.context.setData({
-          gasStationList: gasStationList
         })
-      }
+      },
     })
   }
+  //跳转至导航页面
   toThere(e) {
     console.log(e);
+    let gasStation = e.currentTarget.dataset.gasStation;
+    wx.navigateTo({
+      url: '../../navigation/navigation?gasStation=' + JSON.stringify(gasStation),
+    })
+
   }
 }
 export default GasStation;
